c02d0d93742d49957462857fdd36dea4e03a05b6
InSleepMode <vkurganov05@gmail.com>
2026-07-30 23:46:54 +0200

    - type: feature - name: users - author: InSleepMode

---
+++ internal/features/users/service/service.go
@@ -0,0 +1,49 @@
+package users_service
+
+import (
+	"context"
+
+	"github.com/InSleepMode/golang-todoapp/internal/core/domain"
+)
+
+type UsersService struct {
+	usersRepository UsersRepository
+}
+
+func (s *UsersService) Routes() any {
+	panic("unimplemented")
+}
+
+type UsersRepository interface {
+	CreateUser(
+		ctx context.Context,
+		user domain.User,
+	) (domain.User, error)
+
+	GetUsers(
+		ctx context.Context,
+		limit *int,
+		offset *int,
+	) ([]domain.User, error)
+	GetUser(
+		ctx context.Context,
+		id int,
+	) (domain.User, error)
+	DeleteUser(
+		ctx context.Context,
+		id int,
+	) error
+	PatchUser(
+		ctx context.Context,
+		id int,
+		user domain.User,
+	) (domain.User, error)
+}
+
+func NewUsersService(
+	usersRepository UsersRepository,
+) *UsersService {
+	return &UsersService{
+		usersRepository: usersRepository,
+	}
+}
