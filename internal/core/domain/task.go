e44df1bd62b4c4d3bfb15e03069faf592cdf1e0d
InSleepMode <vkurganov05@gmail.com>
2026-08-05 16:43:57 +0200

    feature/statistics

--- internal/core/domain/task.go
+++ internal/core/domain/task.go
@@ -60,6 +60,20 @@ func NewTaskUninitalized(
 	)
 }
 
+func (t *Task) CompletionDuration() *time.Duration {
+	if !t.Completed {
+		return nil
+	}
+
+	if t.CompletedAt == nil {
+		return nil
+	}
+
+	duration := t.CompletedAt.Sub(t.CreatedAt)
+
+	return &duration
+}
+
 func (t *Task) Validate() error {
 	titleLen := len([]rune(t.Title))
 	if titleLen < 1 || titleLen > 100 {
