fef957280f634dfadd31814edb168eb418b8e072
InSleepMode <vkurganov05@gmail.com>
2026-08-07 14:36:04 +0200

    added frontend

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -22,6 +22,9 @@ import (
 	users_postgres_repository "github.com/InSleepMode/golang-todoapp/internal/features/users/repository/postgres"
 	users_service "github.com/InSleepMode/golang-todoapp/internal/features/users/service"
 	users_transport_http "github.com/InSleepMode/golang-todoapp/internal/features/users/transport/http"
+	web_fs_repository "github.com/InSleepMode/golang-todoapp/internal/features/web/repository/file_system"
+	web_service "github.com/InSleepMode/golang-todoapp/internal/features/web/service"
+	web_transport_http "github.com/InSleepMode/golang-todoapp/internal/features/web/transport/http"
 	"go.uber.org/zap"
 
 	_ "github.com/InSleepMode/golang-todoapp/docs"
@@ -79,6 +82,11 @@ func main() {
 	statisticsService := statistics_service.NewStaticsticsService(statisticsRepository)
 	statisticsTransportHTTP := statistics_transport_http.NewStaticsticsHTTPHandler(statisticsService)
 
+	logger.Debug("initializing feature", zap.String("feature", "web"))
+	webRepository := web_fs_repository.NewWebRepository()
+	webService := web_service.NewWebService(webRepository)
+	webTransportHTTP := web_transport_http.NewWebHTTPHandler(webService)
+
 	logger.Debug("initializing HTTP server")
 	httpServer := core_http_server.NewHTTPServer(
 		core_http_server.NewConfigMust(),
@@ -99,6 +107,7 @@ func main() {
 		apiVersionRouterV1,
 	)
 
+	httpServer.RegisterRoutes(webTransportHTTP.Routes()...)
 	httpServer.RegisterSwagger()
 
 	if err := httpServer.Run(ctx); err != nil {
